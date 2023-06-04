ea8392aa6c177dbba82276a69ddedf7f6696d9d6
Aswin Rajan <aswinachu3333@gmail.com>
2023-06-04 17:31:51 +0530

    Implemented openweatherAPI

--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,46 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const axios = require('axios');
-const openweathermap = require('openweathermap');
 
-
-router.get('/', (req, res)=> {
+router.get('/', (req, res) => {
   res.render('index');
-})
+});
 
-router.post('/cities', async (req, res)=> {
+router.post('/', async (req, res) => {
   const city = req.body.city;
   const apiKey = process.env.OPENWEATHERMAP_API_KEY;
-  
+
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
-  const response = await axios.get(url);
-  if(response){
+  try {
+    const response = await axios.get(url);
 
-    console.log(response,"asassasas");
+    if (response) {
 
-    //Temp
-    let temperature = response.data.main.temp
-    temperature = ((temperature - 32) * 5/9).toFixed(2);
+      // Process the weather data
+      let temperature = response.data.main.temp;
+      temperature = ((temperature - 32) * 5 / 9).toFixed(2);
 
-    //Date
-    let date = new Date();
-    let formattedDate = `${date.getDate()}/ ${date.getMonth() + 1}/ ${date.getFullYear()}`;
+      let date = new Date();
+      let formattedDate = `${date.getDate()}/ ${date.getMonth() + 1}/ ${date.getFullYear()}`;
 
-    //Country
-    let code = response.data.sys.country;
+      let code = response.data.sys.country;
 
-    res.render('index', {city: city, 
-      temperature,weather: response.data.weather[0].description, 
-      humidity: response.data.main.humidity,
-      formattedDate,
-      code,
-      wind: response.data.wind.speed,
-      desc: response.data.weather[0].main,
-    });
-  }else{
-    res.redirect('/');
+      res.render('index', {
+        city: city,
+        temperature: temperature,
+        weather: response.data.weather[0].description,
+        humidity: response.data.main.humidity,
+        formattedDate,
+        code,
+        wind: response.data.wind.speed,
+        desc: response.data.weather[0].main,
+      });
+    }
+  } catch (error) {
+    res.render('index', {cityError:'An error occurred. Please enter a valid city name' });
   }
- 
+
 });
 
 module.exports = router;
